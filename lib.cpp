e8137d2889045be0d2911314c486c9181d42c3e6
EdoardoMaestri <115471996+EdoardoMaestri@users.noreply.github.com>
2022-12-05 17:39:15 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
 
+#include "lett.h"
+
+using namespace std;
+
+bool lettera (char &a)
+{
+    if(((a>=65)&&(a<=90))||((a>=97)&&(a<=122)))
+    {
+        if ((a >= 65) && (a <= 90)) {
+            a = (a + 32);
+        } else if ((a >= 97) && (a <= 122)) {
+            a = (a - 32);
+        } return true;
+    }
+    else
+        return false;
+}
